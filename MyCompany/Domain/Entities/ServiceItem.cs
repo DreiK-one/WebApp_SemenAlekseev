c5fd8aea78e8cf21d80cf57dffde1eb3836fb591
DreiK-one <Dreikone1@mail.ru>
2021-10-13 17:23:02 +0200

    Add local files (from 1 to 4 lesson)

---
+++ MyCompany/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MyCompany.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Fill in the name of the service")]
+        [Display(Name = "Name of the service")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Brief description of the service")]
+        public override string Subtitle { get; set; }
+
+        [Display(Name = "Full description of the service")]
+        public override string Text { get; set; }
+    }
+}
---
+++ MyCompany/Domain/Entities/TextField.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MyCompany.Domain.Entities
+{
+    public class TextField : EntityBase
+    {
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Page name (title)")]
+        public override string Title { get; set; } = "Information page";
+
+        [Display(Name = "Page Content")]
+        public override string Text { get; set; } = "The content is filled in by the administrator";
+    }
+}
